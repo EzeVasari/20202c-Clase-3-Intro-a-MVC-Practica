05e27eb1032c541fef66bea395b1b6fec35d54c1
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-09-17 11:56:18 -0300

    Se crea servicio de Alimentos

---
+++ AlimentosCRUD/AlimentosCRUD/Services/AlimentoService.cs
@@ -0,0 +1,18 @@
+﻿using AlimentosCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlimentosCRUD.Services
+{
+    public class AlimentoService
+    {
+        public static List<Alimento> listaAlimentos = new List<Alimento>();
+
+        public List<Alimento> ObtenerAlimentos()
+        {
+            return listaAlimentos;
+        }
+    }
+}
