1c35fb5f6a4925cf7667f014ed5b031db1475869
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-09-17 11:57:01 -0300

    Se crea controlador de Alimento

---
+++ AlimentosCRUD/AlimentosCRUD/Controllers/AlimentoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AlimentosCRUD.Controllers
+{
+    public class AlimentoController : Controller
+    {
+        // GET: Alimento
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
