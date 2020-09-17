0a85573de7b0a8eb2b46d93eb968f699e42c306f
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2020-09-17 11:56:42 -0300

    Se agrega la clase Alimento

---
+++ AlimentosCRUD/AlimentosCRUD/Models/Alimento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlimentosCRUD.Models
+{
+    public class Alimento
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
